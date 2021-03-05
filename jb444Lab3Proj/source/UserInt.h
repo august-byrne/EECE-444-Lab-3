d2e9d19ff652d9f19b26e0fe7310b7dcb0f5452f
givensm2 <givensm2@wwu.edu>
2021-03-05 13:41:31 -0800

    Tasks and Files Created

--- jb444Lab3Proj/source/UserInt.h
+++ jb444Lab3Proj/source/UserInt.h
@@ -8,6 +8,20 @@
 #ifndef USERINT_H_
 #define USERINT_H_
 
+<<<<<<< Upstream, based on origin/master
 void UIInit(void);
+=======
+
+#define R_ON 1
+#define R_OFF 0
+#define EN_ON 1
+#define EN_OFF 0
+
+void UIInit(void);
+
+INT32U SWCountPend(INT16U tout, OS_ERR *os_err);
+
+void SWCounterCntrlSet(INT8U enable, INT8U reset);
+>>>>>>> d909694 Tasks and Files Created
 
 #endif /* USERINT_H_ */
