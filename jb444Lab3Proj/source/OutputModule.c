89fdee014ae64e88f12865260b295f00542fac71
binderj <binderj@wwu.edu>
2021-03-13 19:35:19 -0800

    Revert "Merge branch 'Jacob's_Branch' into 'master'"
    This reverts merge request !6

--- jb444Lab3Proj/source/OutputModule.c
+++ jb444Lab3Proj/source/OutputModule.c
@@ -3,10 +3,6 @@
 * 10 to 10k Hz. It will be taking taking in the volume, which controls the volume of the
 * Sine wave and the duty cycle of the square wave
 *
-*
-* BUGS:
-* -Currently one only one task dominates the other, depending on how the prio is set
-*
 * Jacob Bindernagel
 * 3/11/2021
 *****************************************************************************************/
@@ -51,13 +47,13 @@ static CPU_STK SquareOutputTaskStk[APP_CFG_SQUARE_GEN_STK_SIZE];
 #define SIZE_CODE_16BIT   0x1
 #define NUM_BLOCKS        2
 #define BYTES_PER_SAMPLE  2
-#define SAMPLES_PER_BLOCK 1024
-#define BYTES_PER_BLOCK    (SAMPLES_PER_BLOCK*BYTES_PER_SAMPLE)
-#define BYTES_PER_BUFFER    (NUM_BLOCKS*BYTES_PER_BLOCK)
+#define SAMPLES_PER_BLOCK 100
+#define BYTES_PER_BLOCK             (SAMPLES_PER_BLOCK*BYTES_PER_SAMPLE)
+#define BYTES_PER_BUFFER            (NUM_BLOCKS*BYTES_PER_BLOCK)
 #define DMA_OUT_CH        0
 #define SAMPLE_PERIOD_Q31 44739
 #define ABS_VAL_MASK       0x7FFFFFFF
-#define DC_OFFSET 2000  //1/2 VFSR + some tweeaking
+#define DC_OFFSET 2047
 
 typedef struct{
     INT8U index;
@@ -69,6 +65,7 @@ typedef struct{
  ******************************************************************************************/
  DMA_BLOCK_RDY dmaInBlockRdy;
  static INT16S DMABuffer[NUM_BLOCKS][SAMPLES_PER_BLOCK];
+
 
 /*****************************************************************************************
 * Task Function Prototypes.
@@ -188,6 +185,9 @@ void OutputInit(void){
                      OS_OPT_TASK_NONE,
                      &os_err);
 
+
+
+
 }
 
 
@@ -209,48 +209,33 @@ static void SineOutputTask(void *p_arg){
     INT16U sample_index = 0;
     INT16U buffer_index;
     q31_t xarg = 0;
-    q31_t xarg_inc;
+    INT32U freq;
+    INT32U vol;
     q31_t sine_value;
-    INT16U freq;
-    INT8U vol;
     STATE mode;
-    (void) p_arg;
 
+    (void) p_arg;
     while(1){
 
 
-        freq = UIFreqGet();
-        vol = UILevGet();
-        mode = UIStateGet();
-
-        DB1_TURN_OFF();
+        buffer_index = DMAPend(0, &os_err);
+        DB1_TURN_ON();
 
         if(mode == SINEWAVE_MODE){
-
-            buffer_index = DMAPend(0, &os_err);
-            xarg_inc = freq*SAMPLE_PERIOD_Q31;
-
         while (sample_index < SAMPLES_PER_BLOCK){
-
             sine_value = arm_sin_q31(xarg); //Computes sine wave value
             arm_mult_q31(&sine_value,&AC_FACTOR,&sine_value,1); //Multiplies by 1/20 of the volume (1.5/(3.3*20))
             sine_value = ((sine_value*vol) >> 20) + DC_OFFSET; //applies volume level, shifts to 12 bits, and applies DC offset
 
             DMABuffer[buffer_index][sample_index] = (INT16S)sine_value;
 
-            xarg = xarg + xarg_inc;
+            xarg = xarg + (freq*SAMPLE_PERIOD_Q31); //Increments counter
             xarg = xarg & ABS_VAL_MASK; //Masks sign bit for roll over
             sample_index++;
         }
         sample_index = 0;
-
+        DB1_TURN_OFF();
         }
-        else{
-
-        }
-
-        DB1_TURN_ON();
-
     }
 
 }
@@ -267,9 +252,10 @@ static void SineOutputTask(void *p_arg){
 
     static void  SquareOutputTask(void *p_arg){
 
+        OS_ERR os_err;
+        INT16U freq;
         INT16U mod;
         INT32U duty;
-        INT16U freq;
         INT8U vol;
         STATE mode;
         (void) p_arg;
@@ -277,11 +263,8 @@ static void SineOutputTask(void *p_arg){
         while(1){
 
 
-            freq = UIFreqGet();
-            vol = UILevGet();
-            mode = UIStateGet();
 
-
+            DB0_TURN_ON();
 
             if(mode == PULSETRAIN_MODE){
 
@@ -320,9 +303,10 @@ static void SineOutputTask(void *p_arg){
                 //Set signal pulse width (duty cycle)
                 FTM3->CONTROLS[3].CnV = FTM_CnV_VAL((INT16U)duty);
 
+                DB0_TURN_OFF();
                 }
             else{
-
+                DB0_TURN_OFF();
             }
             }
 
@@ -336,9 +320,11 @@ static void SineOutputTask(void *p_arg){
 void DMA0_DMA16_IRQHandler(void){
     OS_ERR os_err;
     OSIntEnter();
+    DB1_TURN_ON();
     DMA0->CINT = DMA_CINT_CINT(DMA_OUT_CH);
     dmaInBlockRdy.index ^= 1;                            //toggle buffer index
     OSSemPost(&(dmaInBlockRdy.flag),OS_OPT_POST_1,&os_err);
+    DB1_TURN_OFF();
     OSIntExit();
 }
 
